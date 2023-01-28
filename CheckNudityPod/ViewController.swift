d2ae662019c00aeacb04acc519a5b62758581adb
Ajay <ajay@appringer.com>
2023-01-28 15:38:43 +0530

    Remove VC Code

--- CheckNudityPod/ViewController.swift
+++ CheckNudityPod/ViewController.swift
@@ -8,13 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        NudityModel.checkLocalVideoUrlNudity(with: "file:///Users/imac/Documents/Projects/CheckNudityiOS/CheckNudityPod/videoplayback.mp4", securityLevel: .mid) { nsfwValue, sfwValue in
-            print("nsfwValue: ", nsfwValue)
-            print("sfwValue: ", sfwValue)
-        }
+        
     }
 }
 
